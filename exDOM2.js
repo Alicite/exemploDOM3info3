e68e9581f6d7413fa5caec2fca1bbc96b49cf0ba
Alicite <lunacarter1503@gmail.com>
2026-06-01 21:58:00 -0300

    Resolução da atividade com bônus

--- exDOM2.js
+++ exDOM2.js
@@ -1,16 +1,53 @@
 const lista = document.querySelector('#lista');
+const btAdd = document.querySelector('#bt-adicionar');
+const inputTexto = document.querySelector('#texto');
 
-setTimeout(() => {
+const verificarBD = () => {
+    const dados = localStorage.getItem('item');
+
+    if (!dados) {
+        return [];
+    } else {
+        const resposta = JSON.parse(dados);   
+        return resposta;
+    }
+};
+
+const renderItem = (texto) => {    
     const item = document.createElement('li');
-    item.innerText = "Item 1 do 3info";
+    item.innerText = texto;
     const btRemover = document.createElement('input');
     btRemover.type = 'button';
     btRemover.value = "Remover";
     btRemover.onclick = (evento) => {
         const pai = evento.target.parentElement;
         lista.removeChild(pai);
-    }
 
+        tarefas.splice(tarefas.indexOf(texto), 1);
+        localStorage.setItem('item', JSON.stringify(tarefas))
+    }
+    
     item.appendChild(btRemover);
     lista.appendChild(item);
-}, 3000)
+}
+
+const addItem = () => {
+    const texto = inputTexto.value;
+    
+    renderItem(texto)
+    
+    inputTexto.value = '';
+
+    tarefas.push(texto);
+    localStorage.setItem('item', JSON.stringify(tarefas));
+}
+
+const tarefas = verificarBD();
+tarefas.forEach((item) => renderItem(item));
+
+btAdd.addEventListener('click', addItem);
+inputTexto.addEventListener('keyup', (evento) => {
+    if (evento.key === "Enter") {
+        addItem();
+    }
+});
